e5550f24e2f8a718e0c94cadd34a6629b32743fc
Brian Dennis <briandennis321@gmail.com>
2016-06-04 15:20:26 -0500

    add format.js

---
+++ format.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const portraits = 'images/portraits/';
+const artistic = 'images/artistic/';
+
+fs.readdir(`${portraits}`, (err, files) => {
+  files.forEach( (fileName) => {
+    rename = fileName.split(' ').join('');
+    fs.rename(`${portraits}/${fileName}`, `${portraits}${rename}`)
+    var imageHtml = `<div class="6u"><span class="image fit"><a href="${portraits}${rename}" data-lightbox="${rename}"><img src="${portraits}${rename}" alt="" /></a></span></div>`;
+    fs.appendFile('./formatOutput/portraits.txt', imageHtml);
+  });
+});
+
+fs.readdir(`${artistic}/`, (err, files) => {
+  files.forEach( (fileName) => {
+    var imageHtml = `<div class="6u"><span class="image fit"><a href="${artistic}/${rename}" data-lightbox="${rename}"><img src="${artistic}/${rename}" alt="" /></a></span></div>`;
+    fs.appendFile('./formatOutput/artistic.txt', imageHtml);
+  });
+});
